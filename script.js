83f5d0f82b84f343d129e28169651c21319cd774
JaimeGonzalezLucio <61289546+JaimeGonzalezLucio@users.noreply.github.com>
2023-07-24 08:53:37 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,122 @@
+//menu
+
+const menu = document.getElementById('menu')
+const listItems = document.getElementById('list_items')    
+const items = document.querySelectorAll('.list_item')
+
+    menu.addEventListener('click', function() {        
+        listItems.classList.toggle('nav__items--show')
+        menu.classList.toggle('open')        
+    })
+
+    items.forEach(function(item) {
+        item.addEventListener('click', function() {        
+            listItems.classList.remove('nav__items--show')
+            menu.classList.toggle('open')       
+        })
+    });
+
+//text
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+function startAnimation() {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    const h1Element = document.querySelector("h1");
+    h1Element.innerText = h1Element.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return h1Element.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= h1Element.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+}    
+
+//galery
+
+//galery
+
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  let nextPercentageUnconstrained =0
+  let nextPercentage =0
+  let percentage = 0
+
+    percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -85);
+
+    if (isNaN(nextPercentage)) {
+      nextPercentage=0
+    }
+
+    if (isNaN(nextPercentageUnconstrained)) {
+      nextPercentageUnconstrained=0
+    }
+
+    if (isNaN(percentage)) {
+      percentage=0
+    }
+
+  track.dataset.percentage = nextPercentage;
+
+  track.animate({
+    
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+
+
+setInterval(startAnimation, 3000);
+console.log("R u ok?");
